44fa2577947739b94c7d7a45bd2e3c7c96f4ce66
Bhumika Saxena <bhumikasaxena4590@gmail.com>
2026-02-05 13:47:41 +0530

    new paths

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "appbackend-production-b66a.up.railway.app";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://appbackend-production-b66a.up.railway.app";
